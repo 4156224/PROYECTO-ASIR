01e34d82a795b00d31083c71a89448d393a3787c
4156224 <4156224@alu.murciaeduca.es>
2026-01-30 17:34:53 +0100

    sql_apache2.sh

--- sql_apache2.sh
+++ sql_apache2.sh
@@ -5,4 +5,15 @@ mysql
 CREATE USER 'master'@'localhost' IDENTIFIED BY 'proyecto.master';
 GRANT ALL PRIVILEGES on *.* to 'master'@'localhost';
 FLUSH PRIVILEGES;
-
+quit
+nano /etc/network/interfaces > echo " auto enp0s3
+iface enp0s3 inet static
+ address 172.116.1.13
+ gateway 172.116.1.1
+ dns-nameservers 172.116.1.12 8.8.8.8"
+ systemctl service restart networking
+ mkdir /var/www/html/paginawebnegocio
+ touch /var/www/html/paginawebnegocio/index.html
+ touch /var/www/html/paginawebnegocio/login.html
+ #CREAR VIRTUALHOST Y DIRECTORIO PARA LA CARPETA PAGINAWEBNEGOCIO PARA ACCEDER A LA MISMA
+ 
