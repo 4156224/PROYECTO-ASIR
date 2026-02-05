ed8b46d3d4c469806cc039e7d3dca5ff1952ed32
4156224 <4156224@alu.murciaeduca.es>
2026-02-05 23:32:15 +0100

    Delete installdns.sh

--- installdns.sh
+++
@@ -1,9 +0,0 @@
-apt install bind9
-#MODIFICAR INTERFACES DE RED
-nano /etc/network/interfaces > echo " auto enp0s3
-iface enp0s3 inet static
- address 172.116.1.12
- gateway 172.116.1.1
- dns-nameservers 127.0.0.1 8.8.8.8"
- systemctl service restart networking
- 
