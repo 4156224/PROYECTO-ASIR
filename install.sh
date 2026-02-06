37acde252ee6ac4c2bd26290edcecab3897a0406
4156224 <4156224@alu.murciaeduca.es>
2026-02-06 17:47:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,11 @@ fi
 #INSTALAR DHCP
 instalar_dhcp(){
   apt install isc-dhcp-server -y
-}
+  echo " subnet 192.168.10.0 netmask 255.255.255.0 {
+  range 192.168.10.20 192.168.10.100;
+  options routers 192.168.10.1;
+  options domain-name-servers 8.8.8.8 8.8.4.4 10.0.0.6
+}" >> /etc/dhcp/dhcp.conf
 #-------------------------------------------------
 #INSTALAR DNS
 instalar_dns(){
@@ -25,6 +29,7 @@ instalar_router(){
 #INSTALAR APACHEBBDD
 instalar_apachebbdd(){
   apt install apache2 phpmyadmin mariadb-server -y
+  
 }
 #-------------------------------------------------
 #MENU DE OPCIONES
