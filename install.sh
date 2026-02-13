bf886588c765f747d6ae9bb358bed932d02d9d4d
4156224 <4156224@alu.murciaeduca.es>
2026-02-13 16:54:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,27 +9,49 @@ fi
 #-------------------------------------------------
 #INSTALAR DHCP
 instalar_dhcp(){
-  apt install isc-dhcp-server -y
+  dhcp=$(apt install isc-dhcp-server -y)
+  echo "$dhcp"
   echo " subnet 192.168.10.0 netmask 255.255.255.0 {
   range 192.168.10.20 192.168.10.100;
   options routers 192.168.10.1;
   options domain-name-servers 8.8.8.8 8.8.4.4 10.0.0.6
-}" >> /etc/dhcp/dhcp.conf
+  }" > /etc/dhcp/dhcp.conf
 #-------------------------------------------------
 #INSTALAR DNS
 instalar_dns(){
-  apt install bind9 -y
+  dns=$(apt install bind9 -y)
+  echo "$dns"
+  
 }
 #-------------------------------------------------
 #INSTALAR ROUTER
 instalar_router(){
+  router=$(apt install squid iptables -y)
+  echo "$router"
+  iptablespostrouting=$(iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o ens18 -j MASQUERADE)
+  variable1=$(echo 1 > /proc/sys/net/ipv4/ip_forward)
+  echo "$variable1"
 
 }
 #-------------------------------------------------
 #INSTALAR APACHEBBDD
 instalar_apachebbdd(){
-  apt install apache2 phpmyadmin mariadb-server -y
-  
+  apachebbdd=$(apt install apache2 phpmyadmin mariadb-server -y)
+  echo "$apachebbdd"
+  usuario_root="root"
+  passwd_root="admin"
+  nuevo_usuario="administrador"
+  passwd="administrador"
+  host="localhost"
+  #SQL
+  sql="GRANT ALL PRIVILEGES ON *.* TO '$nuevo_usuario'@'$host' IDENTIFIED BY '$passwd' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+  #EJECUTAR SQL
+  mariadb -u"$usuario_root" -p"$passwd_root" -e "$sql"
+  if [ $? -eq 0 ]; then
+    echo "Superusuario $nuevo_usuario creado exitosamente."
+  else
+    echo "Error al crear el usuario."
+  fi
 }
 #-------------------------------------------------
 #MENU DE OPCIONES
